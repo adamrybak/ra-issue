9d1bc3eadcc25b98a53175d1ab3b3a243236bb81
Adam Rybak <adam.rybak@costagroup.com.au>
2023-03-03 11:01:44 +1100

    the issue commit

---
+++ RaIssue/TestFunc.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace RaIssue
+{
+    public class TestFunc
+    {
+        private readonly ILogger _log;
+
+        public TestFunc(ILoggerFactory loggerFactory)
+        {
+            _log = loggerFactory.CreateLogger<TestFunc>();
+        }
+
+        [FunctionName(nameof(TestFunc))]
+        public void RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+        {
+            _log.LogInformation("Hello World!");
+        }
+
+    }
+}
